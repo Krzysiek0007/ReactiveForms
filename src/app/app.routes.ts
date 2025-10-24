ca4a65f85c979442d95698a8547595d7bfc3d687
adop\krzysztof <krzysztof.jackowski@adop.com.pl>
2025-10-24 10:23:07 +0200

    Dodanie widoku NotFound dla path **

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
-import {Home} from './home/home';
-import {Done} from './done/done';
+import { Home } from './home/home';
+import { Done } from './done/done';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
-{
+  {
+    path: '',
+    component: Home,
+    title: 'Home page',
+  },
+  {
     path: 'form',
     component: Home,
     title: 'Home page',
@@ -13,5 +19,5 @@ export const routes: Routes = [
     component: Done,
     title: 'Done page',
   },
+  { path: '**', component: NotFound },
 ];
-
